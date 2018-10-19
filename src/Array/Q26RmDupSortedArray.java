71836cdb160cd6074a120f7fdc571e710c30f003
Dai, Stephen <stephen.dai@td.com>
2018-10-19 14:05:34 -0400

    add Q26 Q27

---
+++ src/Array/Q26RmDupSortedArray.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class Q26RmDupSortedArray {
+    public static void main(String[] args){
+        int[] nums = new int[]{1,1};
+        System.out.println(removeDuplicates(nums));
+    }
+    public static int removeDuplicates(int[] nums) {
+        //problem requires in-place work, therefore loops through and swap values
+        //corner cases
+        if (nums.length<=1)
+            return nums.length;
+
+        int i=0; //i stands for the left pivot starting point of dup
+        int j=1; //j stands for the right pivot ending point of dup
+        int k=1; //k stands for the left most available position
+
+        while (j < nums.length){
+            while (j<nums.length && nums[j] == nums[i] ){
+                j++;
+            }
+            if(j<nums.length){
+                nums[k] = nums [j];
+                k++;
+
+                i=j;
+                j++;
+            }
+        }
+        return k;
+
+
+    }
+}
---
+++ src/Array/Q27RmElement.java
@@ -0,0 +1,36 @@
+package Array;
+
+public class Q27RmElement {
+    public static void main(String[] args){
+        int [] nums = new int[]{4,5};
+        System.out.println (removeElement(nums, 4));
+    }
+    public static int removeElement(int[] nums, int val){
+        if (nums.length == 0)
+            return 0;
+        //setup left and right pivot, left for looping, right for swapping
+        int i = 0;
+        int j = nums.length -1;
+
+        while (nums[j] == val){
+            j--;
+            if(j < 0)
+                return 0;
+        }
+
+        while (i<j){
+            if(nums[i] == val){
+                nums[i] = nums [j];
+                j--;
+                while (nums[j] == val){
+                    j--;
+                    if(j < 0)
+                        return 0;
+                }
+            }
+            i++;
+        }
+        return (i==j) ? i+1:i;
+
+    }
+}
