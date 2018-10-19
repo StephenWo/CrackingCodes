6196fafae05c9f9ebbfa093f59999e6a68e64a4b
Dai, Stephen <stephen.dai@td.com>
2018-10-19 15:21:41 -0400

    Q35 resolved

---
+++ src/Array/Q35searchInsert.java
@@ -0,0 +1,35 @@
+package Array;
+
+public class Q35searchInsert {
+    public static void main(String[] args){
+        int[] nums = new int[]{1,3,5,6};
+        System.out.println(searchInsert(nums, 2));
+
+    }
+    public static int searchInsert(int[] nums, int target) {
+        //this is binary search
+        //complexity Ologn
+        //highlights return i++ will return i
+        if (nums.length==0)
+            return 0;
+
+
+        return searchHelper(nums,target, 0, nums.length -1);
+    }
+
+    private static int searchHelper(int[] nums, int target, int left, int right){
+        int mid = left + (right - left)/2;
+        if (right - left == 0){
+            return nums[left] >= target ? left:left+1;
+        }
+
+        if (nums[mid] == target){
+            return mid;
+        }else if (nums[mid] > target){
+            return searchHelper(nums, target, left, mid);
+        }else {
+            return searchHelper(nums, target, ++mid, right);
+        }
+
+    }
+}
